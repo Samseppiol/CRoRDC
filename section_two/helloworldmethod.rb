5a5859e8c8d561528be86cd7abb33fbbe50d2169
Samseppiol <j_hage@live.com>
2017-03-28 20:30:14 +1100

    First push for this project, three different ways to say HELLO WORLD in ruby.

---
+++ section_two/helloworldmethod.rb
@@ -0,0 +1,6 @@
+def hello(world)
+  puts world
+end
+
+hello "Hello World, this is a method!"
+hello "Welcome to the complete RoR developer course"
---
+++ section_two/helloworldstring.rb
@@ -0,0 +1,4 @@
+hello = "Hello World"
+puts hello
+hello = "Welcome to the complete ruby on rails developer course"
+puts hello 
