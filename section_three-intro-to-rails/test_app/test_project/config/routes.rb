c32787b91a7c6bf2501c7b25726810fc0df944e7
Samseppiol <j_hage@live.com>
2017-04-07 17:47:41 +1000

    A quick intro to routes and creating and displaying html pages with embedded ruby for links

--- section_three-intro-to-rails/test_app/test_project/config/routes.rb
+++ section_three-intro-to-rails/test_app/test_project/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Configuring our route to the welcome home link
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+
 end
