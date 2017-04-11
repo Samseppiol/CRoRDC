e6da35fbd16936d0981cdca539fef1f0874f1a22
Samseppiol <j_hage@live.com>
2017-04-11 16:15:35 +1000

    Installed bootstrap and made section five directory

---
+++ section_five-styling/alpha_blog/app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ActiveRecord::Base
+  # We discovered in our rails console that if you enter an empty article it will still register to the
+  #database which is very bad. So the point of this model is to add validations to make sure that doesnt happen!
+
+  #We also want to add length validation because if a title and or desacription is one character, that doesn't
+  # make much sense.
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :description, presence: true, length: { minimum: 10, maximum: 300}
+
+end
---
+++ section_five-styling/alpha_blog/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  # This resources code is magical. This gives us all the CRUD elements.
+  resources :articles
+end
