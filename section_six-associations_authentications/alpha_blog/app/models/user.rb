25bb8f7f5f5e9d7b51875617e99d4e70e2bc55d8
Samseppiol <j_hage@live.com>
2017-04-15 23:04:04 +1000

    Added validations to usernames and to email using a regular expression

--- section_six-associations_authentications/alpha_blog/app/models/user.rb
+++ section_six-associations_authentications/alpha_blog/app/models/user.rb
@@ -1,4 +1,10 @@
 class User <ActiveRecord::Base
+  validates :username, presence: true, uniqueness: { case_sensitive: false},
+            length: { minimum: 4, maximum: 20 }
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, length: { maximum: 100},
+            uniqueness: { case_sensitive: false},
+            format: { with: VALID_EMAIL_REGEX }
 
 end
