39a161d16b98cdf737cbd77d29b5442af1de1db0
Samseppiol <j_hage@live.com>
2017-04-17 19:58:53 +1000

    Added secure password and attributes to user

--- section_six-associations_authentications/alpha_blog/app/models/user.rb
+++ section_six-associations_authentications/alpha_blog/app/models/user.rb
@@ -8,5 +8,6 @@ class User <ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 100},
             uniqueness: { case_sensitive: false},
             format: { with: VALID_EMAIL_REGEX }
+            has_secure_password
 
 end
