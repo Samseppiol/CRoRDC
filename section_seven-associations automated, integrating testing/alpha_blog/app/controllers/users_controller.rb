88f0c7a673fe9b2081525e3d41c551ab5ce1a150
Samseppiol <j_hage@live.com>
2017-04-20 19:37:19 +1000

    Push for start of section seven

---
+++ section_seven-associations automated, integrating testing/alpha_blog/app/controllers/users_controller.rb
@@ -0,0 +1,70 @@
+class UsersController <ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the alpha blog #{@user.username}!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account updated succesfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user_articles = @user.articles.paginate(page: params["page"], per_page: 1)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = 'User and all associated articles have been deleted'
+    redirect_to users_path
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user and !current_user.admin?
+      flash[:danger] = "You can only edit your own user THIEF!"
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = 'Only admin users can do that, get lost'
+      redirect_to root_path
+    end
+  end
+end
---
+++ section_seven-associations automated, integrating testing/alpha_blog/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  # This resources code is magical. This gives us all the CRUD elements.
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+end
