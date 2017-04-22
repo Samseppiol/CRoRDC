712772cbb96cc303eeaada9bc78f9392eb34b941
Samseppiol <j_hage@live.com>
2017-04-22 22:42:38 +1000

    Added edit functionality to categories and ensured only admins can do this

--- section_seven-associations automated, integrating testing/alpha_blog/app/controllers/categories_controller.rb
+++ section_seven-associations automated, integrating testing/alpha_blog/app/controllers/categories_controller.rb
@@ -20,6 +20,20 @@ class CategoriesController <ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = 'Name was successfully changed'
+      redirect_to category_path(@category)
+    else
+
+    end
+  end
+
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
