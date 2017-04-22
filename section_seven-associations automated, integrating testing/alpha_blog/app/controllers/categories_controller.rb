bacb9b333bf85cc544b8f60f4acb5e4ecd6bf5f5
Samseppiol <j_hage@live.com>
2017-04-22 12:40:28 +1000

    Add admin rights only to create categories

--- section_seven-associations automated, integrating testing/alpha_blog/app/controllers/categories_controller.rb
+++ section_seven-associations automated, integrating testing/alpha_blog/app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController <ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -26,5 +27,12 @@ class CategoriesController <ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = 'Only admins can do that'
+      redirect_to categories_path
+    end
   end
 end
--- section_seven-associations automated, integrating testing/alpha_blog/test/controllers/categories_controller_test.rb
+++ section_seven-associations automated, integrating testing/alpha_blog/test/controllers/categories_controller_test.rb
@@ -4,13 +4,16 @@ class CategoriesControllerTest < ActionController::TestCase
 
   def setup
     @category = Category.create(name: 'sports')
+    @user = User.create(username: 'Jack', email: 'jack@jack.com', password: 'password', admin: true)
   end
+
   test 'should get categories index' do
     get :index
     assert_response :success
   end
 
   test 'should get new' do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -20,5 +23,11 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should redirect create when admin not logged in' do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: 'sports' }
+    end
+    assert_redirected_to categories_path
+  end
 
 end
