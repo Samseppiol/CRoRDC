7f2be3e1332bed384db3af51153117eb80fdccaf
Samseppiol <j_hage@live.com>
2017-04-24 14:57:41 +1000

    Added a stock quote gem to bring in stocks at real time, created a users controller, routes and views for my portfolio

---
+++ section_eight-financetracker/finance-tracker/app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+
+  def my_portoflio
+  end
+
+end
--- section_eight-financetracker/finance-tracker/config/routes.rb
+++ section_eight-financetracker/finance-tracker/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
